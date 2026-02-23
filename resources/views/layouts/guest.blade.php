1fd0017066e01c4e4dd80408620a7fd373669708
Davidparra-spoon <practicantespoon01@gmail.com>
2026-02-23 10:49:34 -0500

    corretions of login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,71 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <style>
+    .wave-wrapper {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 180px;
+        overflow: hidden;
+        pointer-events: none;
+        z-index: 1;
+    }
+
+    .wave-container {
+        display: flex;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        /* 1440px * 2 */
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    /* ONDA FRONTAL: Hacia la izquierda */
+    .wave-container.front {
+        left: 0;
+        animation: wave-left 20s linear infinite !important;
+    }
+
+    /* ONDA TRASERA: Hacia la derecha */
+    .wave-container.back {
+        opacity: 0.4;
+        bottom: 50px;
+        left: -100%;
+        /* Empezamos desplazados para el bucle derecho */
+        animation: wave-right 15s linear infinite !important;
+    }
+
+    @keyframes wave-left {
+        0% {
+            transform: translate3d(0, 0, 0);
+        }
+
+        100% {
+            transform: translate3d(-100%, 0, 0);
+        }
+    }
+
+    @keyframes wave-right {
+        0% {
+            transform: translate3d(0, 0, 0);
+        }
+
+        100% {
+            transform: translate3d(100%, 0, 0);
+        }
+    }
+
+    .wave-container svg {
+        width: 100%;
+        height: 100%;
+        display: block;
+        flex-shrink: 0;
+    }
+    </style>
     @stack('styles')
 </head>
 
@@ -25,6 +90,7 @@
         <div class="w-full">
             {{ $slot }}
         </div>
+        @livewireScripts
     </div>
 </body>
 
