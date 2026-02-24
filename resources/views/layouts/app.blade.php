94983f5358e210283c9c657f454140cd51f6f404
Davidparra-spoon <practicantespoon01@gmail.com>
2026-02-24 10:03:14 -0500

    feat: .ico added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="shortcut icon" href="spoon-ico.ico" type="spoon-ico.ico">
     <title>{{ config('app.name', 'Legal') }}</title>
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
